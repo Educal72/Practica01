c02bbbc55b306593ffc604bee943cd141bc08c6b
DYL4N1616 <106412889+DYL4N1616@users.noreply.github.com>
2022-10-23 21:09:44 -0600

    Practica01-Dylan Ureña Piedra

---
+++ src/main/java/com/practica01/domain/Estado.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.practica01.domain;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="estado")
+
+public class Estado implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idPais;
+    int poblacion;
+    String nombre;
+    String capital;
+    int costas ;
+    String continente;
+
+   public Estado() {
+    }
+
+   public Estado(int poblacion,String nombre, String capital, int costas, String continente) {
+        this.poblacion=poblacion;
+        this.nombre = nombre;
+        this.capital = capital;
+        this.costas = costas;
+        this.continente = continente;
+    }
+}
---
+++ src/main/java/com/practica01/service/EstadoService.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.practica01.service;
+
+import com.practica01.domain.Estado;
+import java.util.List;
+
+public interface EstadoService {
+    
+     public List<Estado> getEstados();
+    
+    public Estado getEstados(Estado estado);
+    
+    public void save(Estado estado);
+    
+    public void delete(Estado estado);
+    
+}
